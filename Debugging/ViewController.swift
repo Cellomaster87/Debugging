44374c22b02c324ba5cafc555c72f6bfb7536231
Michele Galvagno <michicello@gmail.com>
2019-04-13 14:21:27 +0200

    Assertions

--- Debugging/ViewController.swift
+++ Debugging/ViewController.swift
@@ -13,10 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(1, 2, 3, 4, 5, separator: "-")
-        print("Some message", terminator: "")
+        assert(1 == 1, "Math failure!")
+        assert(1 == 2, "Math failure!")
     }
-
-
 }
 
